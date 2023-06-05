c3587d352fe0b655a099617b80e58dfc26812510
SidnyZH\Sidny <syzapataho@gmail.com>
2023-06-05 17:05:07 -0500

    -Fixing the problems

--- 1842-html-validaciones-proyecto_inicial/assets/js/app.js
+++ 1842-html-validaciones-proyecto_inicial/assets/js/app.js
@@ -1,10 +1,10 @@
-import {valida} from "./validaciones.js";
+import { valida } from "./validaciones.js";
 
 //Listenes cada vez que el usiario salga de es einput 
 //selñecciona todas las etiquetas input que hay en el HTML
 const inputs = document.querySelectorAll("input");
 //a cada uno de esos inputs la va a llamar la fución validar cuando salga del foco (blur)
-inputs.forEach( input => {
+inputs.forEach( (input) => {
     input.addEventListener("blur", (input) => {
         valida(input.target);
     });
--- 1842-html-validaciones-proyecto_inicial/assets/js/validaciones.js
+++ 1842-html-validaciones-proyecto_inicial/assets/js/validaciones.js
@@ -1,50 +1,63 @@
-export function valida(input){
+export function valida(input) {
     const tipoDeInput = input.dataset.tipo;
-    if(validadores[tipoDeInput]){
-        validadores[tipoDeInput](input);
+    if (validadores[tipoDeInput]) {
+      validadores[tipoDeInput](input);
     }
-
-    if(input.validity.valid){
-        input.parentElement.classList.remove("input-container--invalid");
-        input.parentElement.querySelector(".input.message-error").innerHTML = ""
-    }
-    else{
-        input.parentElement.classList.add("input-container--invalid");
-        input.parentElement.querySelector(".input.message-error").innerHTML = 
+  
+    if (input.validity.valid) {
+      input.parentElement.classList.remove("input-container--invalid");
+      input.parentElement.querySelector(".input-message-error").innerHTML = "";
+    } else {
+      input.parentElement.classList.add("input-container--invalid");
+      input.parentElement.querySelector(".input-message-error").innerHTML =
         mostrarMensajeDeError(tipoDeInput, input);
     }
-}
+  }
 
-const tipoDeErrores = [
-   "typeMismatch",
-   "valueMissing",
-   "patterMismatch",
-   "customError",
-];
+  const tipoDeErrores = [
+    "valueMissing",
+    "typeMismatch",
+    "patternMismatch",
+    "customError",
+  ];
 
 const mensajesDeError = {
-    nombre: {
-        valueMissing: "Este campo no puede estar vacio"
-    },
-     email: {
-            valueMissing:  "Este campo no puede estar vacio",
-            typeMismatch:  "El correo no es valido"
-        },
-    password: {
-        valueMissing:  "Este campo no puede estar vacio",
-        patterMismatch:  "debe contener al menos 8 caracteres, maximo 15, una letra mayuscula, una minuscila, un dígito, no espacios en blanco, 1 caracter epecial"
-
-    },
-    nacimiento: {
-        valueMissing:  "Este campo no puede estar vacio",
-        customError: "Debes tener al menos 18 años de edad",
-
-    },
-   
-}
-const validadores = {
-    nacimiento: (input) => ValidarNacimiento(input),
+  nombre: {
+    valueMissing: "El campo nombre no puede estar vacío",
+  },
+  email: {
+    valueMissing: "El campo correo no puede estar vacío",
+    typeMismatch: "El correo no es válido",
+  },
+  password: {
+    valueMissing: "El campo contraseña no puede estar vacío",
+    patternMismatch:
+    "debe contener al menos 8 caracteres, maximo 15, una letra mayuscula, una minuscila, un dígito, no espacios en blanco, 1 caracter epecial",
+  },
+  nacimiento: {
+    valueMissing: "Este campo no puede estar vacío",
+    customError: "Debes tener al menos 18 años de edad",
+  },
+  numero: {
+    valueMissing: "Este campo no puede estar vacío",
+    patternMismatch: "El formato requerido es XXXXXXXXXX 10 números",
+  },
+  direccion: {
+    valueMissing: "Este campo no puede estar vacío",
+    patternMismatch: "La dirección debe contener entre 10 a 40 caracteres.",
+  },
+  ciudad: {
+    valueMissing: "Este campo no puede estar vacío",
+    patternMismatch: "La ciudad debe contener entre 10 a 40 caracteres.",
+  },
+  estado: {
+    valueMissing: "Este campo no puede estar vacío",
+    patternMismatch: "El estado debe contener entre 10 a 40 caracteres.",
+  },
 };
+const validadores = {
+    nacimiento: (input) => validarNacimiento(input),
+  };
 
 //BIrth es el id del input de html que quiero seleccionar
 // const inputNacimiento = document.querySelector("#birth");
@@ -55,33 +68,38 @@ const validadores = {
 //     console.log(evento.target)
 // });
 
-function  mostrarMensajeDeError(tipoDeInput, input){
-let mensaje = "" ; 
-tipoDeErrores.forEach(error => {
-    if (input.validity[error]){
+function mostrarMensajeDeError(tipoDeInput, input) {
+    let mensaje = "";
+    tipoDeErrores.forEach((error) => {
+      if (input.validity[error]) {
         console.log(tipoDeInput, error);
         console.log(input.validity[error]);
         console.log(mensajesDeError[tipoDeInput][error]);
-        mensaje =mensajesDeError[tipoDeInput][error];
-    }
-});
-return mensaje; 
-}
+        mensaje = mensajesDeError[tipoDeInput][error];
+      }
+    });
+    return mensaje;
+  }
 //Creo una función que reciba el input 
 //Me interesa acceder al valor del input 
-function ValidarNacimiento(input){
-const fechaCliente = new Date(input.value);
-let mensaje = " "; 
-if (mayorDeEdad(fechaCliente)){
-    mensaje = "Debes tenr al menos 18 años de edad "
-}
-;
-}
+function validarNacimiento(input) {
+    const fechaCliente = new Date(input.value);
+    let mensaje = "";
+    if (!mayorDeEdad(fechaCliente)) {
+      mensaje = "Debes tener al menos 18 años de edad";
+    }
+  
+    input.setCustomValidity(mensaje);
+  }
 //Verificar que la fecha de nacimiento ingresa correspond acon un mayor de edad 
 
-function mayorDeEdad (fecha) {
-    const fechaActual = new Date(); 
-    const diferenciaFechas = new Date(fecha.getUTCFullYear() + 18, fecha.getUTCMonth(),fecha.getUTCDate( ))
+function mayorDeEdad(fecha) {
+    const fechaActual = new Date();
+    const diferenciaFechas = new Date(
+      fecha.getUTCFullYear() + 18,
+      fecha.getUTCMonth(),
+      fecha.getUTCDate()
+    );
     return diferenciaFechas <= fechaActual;
-
-}
+  }
+  
